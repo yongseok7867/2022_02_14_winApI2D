3cfa8f84abf5dee46a3911e6771df4ade9256183
Heo <Heo@DESKTOP-1RQBDFM>
2022-02-25 14:55:58 +0900

    winAPI2D

--- 2022_02_14_winApI2D/2022_02_14_winApI2D.cpp
+++ 2022_02_14_winApI2D/2022_02_14_winApI2D.cpp
@@ -1,7 +1,7 @@
 ﻿// 2022_02_14_winApI2D.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
-#include "framework.h"
+#include "pch.h"
 #include "2022_02_14_winApI2D.h"
 
 #define MAX_LOADSTRING 100
@@ -17,16 +17,27 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// _In_ : SAL 주석 - 자주 사용되는 주석을 매번 적기보다 키워드로 작성함.
+// hInstance : 실행된 프로세스의 시작 주소. 인스턴스 핸들
+// hPrevInstance : 이전에 실행된 인스턴스 핸들
+// lpCmdLine : 명령으로 입력된 프로그램의 인수
+// nCmdShow : 프로그램이 시작될 형태
+
+// 윈도우 메인의 역활
+// 1. 윈도우창 세팅 후 화면에 띄우고
+// 2. 메세지 루프
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
+    // 사용되지 않은 매개변수의 정의
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 여기에 코드를 입력합니다.
 
+    // 리소스 뷰의 String table 용도
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_MY20220214WINAPI2D, szWindowClass, MAX_LOADSTRING);
@@ -37,21 +48,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     {
         return FALSE;
     }
-
+    
+    // 단축키 정부
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY20220214WINAPI2D));
 
-    MSG msg;
+    // 기본 메세지 루프입니다:
+    // 메세지 큐에서 메세지가 확인될 때까지 대기
+    // 메세지 큐에 msg.message == WM_QUIT인 경우 false를 반환
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    // GetMessage : 메세지 큐에 메세지가 없다면 대기, 메세지가 들어온다면 true 반환
+    // PeekMessage : 메세지 큐에 메세지가 없다면  false, 메세지가 있다면 true 반환
+
+    // 게임 루프
+    // GetMessage의 대기 상태 유지에서
+    // 현재 PeekMessage의 메세지가 없는 상황에서 게임 상황 처리
+    MSG msg;
+    while (true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (WM_QUIT == msg.message)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) // 단추키에 대한 처리
+            {
+                TranslateMessage(&msg); // 키보드 입력메세지 처리를 담당
+                DispatchMessage(&msg);  // WndProc에서 전달된 메세지를 실제 윈도우에 전달
+            }
+        }
+        else
+        {
+            // 게임 처리
         }
     }
-
     return (int) msg.wParam;
 }
 
@@ -64,21 +93,22 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
+    // 윈도우 창의 정보를 저장하기 위한 구조체
     WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.cbSize = sizeof(WNDCLASSEX);   // 구조체의 크기 설정
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY20220214WINAPI2D));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY20220214WINAPI2D);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;  // 윈도우 클래스의 스타일 지정
+    wcex.lpfnWndProc    = WndProc;                  // 메세지를 처리하는 함수를 지정(윈도우 프로시져)
+    wcex.cbClsExtra     = 0;                        // 윈도우 클래스에서 사용하고자 하는 여분의 메모리양을 바이트 단위로 지정
+    wcex.cbWndExtra     = 0;                        // cbClsExtra와 유사하나 개별 윈도우에서 사용하고자 하는 여분의 메모리양을 지정
+    wcex.hInstance      = hInstance;                // 윈도우 클래스를 등록한 인스턴스의 핸들
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY20220214WINAPI2D)); // 프로그램 아이콘
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));         // 타이틀바 좌상단과 윈도우가 최소화 되었을 때 보여주는 아이콘을 지정
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);                               // 커서 지정
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);                                     // 윈도우 작업영역애 칠한 배경 브러시
+    wcex.lpszMenuName   = nullptr;                                                      // 윈도우에서 사용할 메뉴 지정, nullptr 로 없앰
+    wcex.lpszClassName  = szWindowClass;                                                // 윈도우 클래스의 이름
 
     return RegisterClassExW(&wcex);
 }
@@ -97,13 +127,31 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass,        // 클래스 이름
+                            szTitle,                // 윈도우 타이틀 String
+                            WINSTYLE,               // 윈도우 스타일
+                            WINSTARTX,              // 윈도우 시작 X
+                            WINSTARTY,              // 윈도우 시작 Y
+                            WINSIZEX,               // 윈도우 가로 크기
+                            WINSIZEY,               // 윈도우 세로 크기
+                            nullptr,                // 부모 윈도우
+                            nullptr,                // 메뉴 핸들
+                            hInstance,              // 프로세스 인스턴스의 핸들
+                            nullptr);               // 추가 매개변수
 
    if (!hWnd)
    {
       return FALSE;
    }
+   
+   RECT rc;
+   rc.left = 0;
+   rc.top = 0;
+   rc.right = WINSIZEX;
+   rc.bottom = WINSIZEY;
+   // 실제 내용 창이 크기에 맞게끔 하도록 사이즈.
+   AdjustWindowRect(&rc, WINSTYLE, false);
+   // 위에서 얻은 사이즈로 윈도우 사이즈를 세팅하자.
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
